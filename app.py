3b0992107bc7661274fe77b64fe67aaef6ef51b0
Lauvanya28 <129969459+Lauvanya28@users.noreply.github.com>
2024-12-11 17:30:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -507,6 +507,6 @@ def current_status():
 if __name__ == '__main__':
     try:
         logger.info("Starting Flask server...")
-        app.run(host='127.0.0.1', port=5000, debug=True, use_reloader=False)
+        app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
     except KeyboardInterrupt:
         logger.info("Shutting down application.")
